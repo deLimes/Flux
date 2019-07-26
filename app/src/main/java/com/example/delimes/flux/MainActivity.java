f0cb5ba2fe56ffaf6020cafc690dbc1dfc7e7232
deLimes <3830123@gmail.com>
2019-07-26 19:18:14 +0300

    fixed some bugs

--- app/src/main/java/com/example/delimes/flux/MainActivity.java
+++ app/src/main/java/com/example/delimes/flux/MainActivity.java
@@ -186,33 +186,6 @@ public class MainActivity extends AppCompatActivity {
         //this.finish();
         //////////////////////////////////////////////
 
-        //////////////////////////////////////////////
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-            // Here, thisActivity is the current activity
-            if (ContextCompat.checkSelfPermission(context, Manifest.permission.RECEIVE_BOOT_COMPLETED)
-                    != PackageManager.PERMISSION_GRANTED) {
-
-                // Permission is not granted
-                // Should we show an explanation?
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                        Manifest.permission.RECEIVE_BOOT_COMPLETED)) {
-                    // Show an explanation to the user *asynchronously* -- don't block
-                    // this thread waiting for the user's response! After the user
-                    // sees the explanation, try again to request the permission.
-                } else {
-                    // No explanation needed; request the permission
-                    ActivityCompat.requestPermissions(this,
-                            new String[]{Manifest.permission.RECEIVE_BOOT_COMPLETED},
-                            MY_PERMISSIONS_REQUEST_RECEIVE_BOOT_COMPLETED);
-
-                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                    // app-defined int constant. The callback method gets the
-                    // result of the request.
-                }
-            }
-        }
-        //////////////////////////////////////////////
 
 
         restoreCyclicTasks();
@@ -268,6 +241,34 @@ public class MainActivity extends AppCompatActivity {
 //            this.finish();
 //            return;
 //        }
+
+        //////////////////////////////////////////////
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            // Here, thisActivity is the current activity
+            if (ContextCompat.checkSelfPermission(context, Manifest.permission.RECEIVE_BOOT_COMPLETED)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Permission is not granted
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.RECEIVE_BOOT_COMPLETED)) {
+                    // Show an explanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+                } else {
+                    // No explanation needed; request the permission
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.RECEIVE_BOOT_COMPLETED},
+                            MY_PERMISSIONS_REQUEST_RECEIVE_BOOT_COMPLETED);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }
+        }
+        //////////////////////////////////////////////
 
 
 
